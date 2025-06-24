0d9cae57f923ba71266de44ef89261d9c4581d6f
Alvaro Tomasino <atomasino08@gmail.com>
2025-06-24 10:33:57 -0300

    cambiando tipo de dato de repository

--- src/shared/repository.ts
+++ src/shared/repository.ts
@@ -1,7 +1,7 @@
-export interface Repository<T>{
-    findAll(): T[] | undefined
-    findOne(item: {id: string}): T | undefined
-    add(item: T): T | undefined
-    update(item: T): T | undefined
-    remove(item:  {id: string}): T | undefined
+export interface Repository<T> {
+  findAll(): T[] | undefined
+  findOne(item: { id: number | string }): T | undefined
+  add(item: T): T | undefined
+  update(item: T): T | undefined
+  remove(item: { id: number | string }): T | undefined
 }
