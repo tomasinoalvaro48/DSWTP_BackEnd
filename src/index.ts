bcf53b0a1283b0e2e0d48d4f8b958f7717aea748
Alvaro Tomasino <atomasino08@gmail.com>
2025-06-25 16:12:10 -0300

    cambio de nombre "app.ts" a "index.ts" y modificacion en "package.json" del script "start:dev"

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import { localidadRouter } from './localidad/localidad.routes.js'
+import { tipoRouter } from './tipo_anomalia/tipo_anomalia.routes.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/api/localidad', localidadRouter)
+app.use('/api/tipo_anomalia', tipoRouter)
+
+app.use((_, res) => {
+  res.status(404).send({ mesagge: 'Resource not found' })
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on https://localhost:3000')
+})
