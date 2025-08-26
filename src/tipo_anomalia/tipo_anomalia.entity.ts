297b12b65720d29acf077521f019fd66d7538d9b
Alvaro Tomasino <atomasino08@gmail.com>
2025-08-26 11:08:05 -0300

    Arreglo CRUD Entity Tipo de Anomalia

--- src/tipo_anomalia/tipo_anomalia.entity.ts
+++ src/tipo_anomalia/tipo_anomalia.entity.ts
@@ -1,14 +1,11 @@
-import { Entity, Property } from '@mikro-orm/core'
+import { Entity, ManyToMany, Property, Collection, OneToMany } from '@mikro-orm/core'
 import { BaseEntity } from '../shared/db/baseEntity.entity.js'
 
 @Entity()
 export class Tipo_Anomalia extends BaseEntity {
-  @Property({ nullable: false, unique: true, autoincrement: true })
-  cod_anom!: number
+  @Property({ nullable: false })
+  nombre_tipo_anomalia!: string
 
   @Property({ nullable: false })
-  nombre_anom!: string
-
-  @Property({ nullable: false })
-  dif_anom!: number
+  dificultad_tipo_anomalia!: number
 }
