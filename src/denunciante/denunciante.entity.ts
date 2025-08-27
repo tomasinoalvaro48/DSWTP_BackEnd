2bc066d1d3973704189b4c3321d545c039b9d49e
Alvaro Tomasino <atomasino08@gmail.com>
2025-08-27 16:40:36 -0300

    fix error in denunciante entity

--- src/denunciante/denunciante.entity.ts
+++ src/denunciante/denunciante.entity.ts
@@ -4,7 +4,6 @@ import { Entity, Property } from '@mikro-orm/core'
 import { BaseEntity } from '../shared/db/baseEntity.entity.js'
 import { Pedido_Resolucion } from '../pedido_resolucion/pedido_resolucion.entity.js'
 
-
 @Entity()
 export class Denunciante extends BaseEntity {
   /*
@@ -15,15 +14,14 @@ export class Denunciante extends BaseEntity {
   nombre_apellido_denunciante!: string
 
   @Property({ nullable: false })
-   telefono_denunciante!: string
+  telefono_denunciante!: string
 
   @Property({ nullable: false })
-   email_denunciante!: string
+  email_denunciante!: string
 
-  
-  @OneToMany(() => Pedido_Resolucion, (pedido_resolucion)=>pedido_resolucion.denunciante,{
-    cascade: [Cascade.ALL], 
-    nullable: true
+  @OneToMany(() => Pedido_Resolucion, (pedido_resolucion) => pedido_resolucion.denunciante, {
+    cascade: [Cascade.ALL],
+    nullable: true,
   })
-  pedidos_resolucion = new Collection<Pedido_Resolucion>(this)*/
+  pedidos_resolucion = new Collection<Pedido_Resolucion>(this)
 }
