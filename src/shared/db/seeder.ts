93465d5f8f35dc99ace0a4564d46817a86c41962
Alvaro Tomasino <alvaroyaugusto@gmail.com>
2025-10-28 11:28:56 -0300

    más datos iniciales agregdos

--- src/shared/db/seeder.ts
+++ src/shared/db/seeder.ts
@@ -3,6 +3,8 @@ import { Localidad } from '../../localidad/localidad.entity.js'
 import { Zona } from '../../localidad/zona.entity.js'
 import { Usuario } from '../../usuario/usuario.entity.js'
 import bcrypt from 'bcryptjs'
+import { Denunciante } from '../../denunciante/denunciante.entity.js'
+import { Tipo_Anomalia } from '../../tipo_anomalia/tipo_anomalia.entity.js'
 
 export async function seedDatabase() {
   const em = orm.em.fork()
@@ -20,23 +22,88 @@ export async function seedDatabase() {
     nombre_localidad: 'Rosario',
     codigo_localidad: '2000',
   })
+  const localidad2 = em.create(Localidad, {
+    nombre_localidad: 'Arroyo Seco',
+    codigo_localidad: '2128',
+  })
+  const localidad3 = em.create(Localidad, {
+    nombre_localidad: 'Santa Fe',
+    codigo_localidad: '3000',
+  })
 
   // Crear Zona
   const zona = em.create(Zona, {
-    nombre_zona: 'Zona Centro',
+    nombre_zona: 'Centro',
     localidad: localidad,
   })
+  em.create(Zona, {
+    nombre_zona: 'Norte',
+    localidad: localidad,
+  })
+  em.create(Zona, {
+    nombre_zona: 'Sur',
+    localidad: localidad,
+  })
+  em.create(Zona, {
+    nombre_zona: 'Arriba',
+    localidad: localidad2,
+  })
+  em.create(Zona, {
+    nombre_zona: 'Abajo',
+    localidad: localidad2,
+  })
+  em.create(Zona, {
+    nombre_zona: 'Acceso',
+    localidad: localidad3,
+  })
 
-  const hashPsw = await bcrypt.hash('operador', 10)
-
-  const usuario = em.create(Usuario, {
+  // Crear Usuarios
+  const hashPsw1 = await bcrypt.hash('123456', 10)
+  const hashPsw2 = await bcrypt.hash('123456', 10)
+  const hashPsw3 = await bcrypt.hash('123456', 10)
+  em.create(Usuario, {
     nombre_usuario: 'operador',
-    email_usuario: 'operador@operador',
-    password_usuario: hashPsw,
+    email_usuario: 'o@o',
+    password_usuario: hashPsw1,
     tipo_usuario: 'operador',
     nivel_cazador: 10,
     estado_aprobacion: 'aprobado',
     zona: zona,
+  })
+  em.create(Usuario, {
+    nombre_usuario: 'Peter Venkman',
+    email_usuario: 'c@c',
+    password_usuario: hashPsw2,
+    tipo_usuario: 'cazador',
+    nivel_cazador: 10,
+    estado_aprobacion: 'aprobado',
+    zona: zona,
+  })
+
+  // Crear Denunciante
+  em.create(Denunciante, {
+    nombre_apellido_denunciante: 'Pepe Gonzalez',
+    email_denunciante: 'd@d',
+    telefono_denunciante: '341-123456',
+    password_denunciante: hashPsw3,
+  })
+
+  // Crear Tipo_Anomalia
+  em.create(Tipo_Anomalia, {
+    nombre_tipo_anomalia: 'Gollum',
+    dificultad_tipo_anomalia: 1,
+  })
+  em.create(Tipo_Anomalia, {
+    nombre_tipo_anomalia: 'Dementor',
+    dificultad_tipo_anomalia: 3,
+  })
+  em.create(Tipo_Anomalia, {
+    nombre_tipo_anomalia: 'Library Ghost',
+    dificultad_tipo_anomalia: 2,
+  })
+  em.create(Tipo_Anomalia, {
+    nombre_tipo_anomalia: 'Basilisk',
+    dificultad_tipo_anomalia: 3,
   })
 
   await em.flush()
