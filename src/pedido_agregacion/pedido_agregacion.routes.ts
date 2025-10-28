bf07208b6d43050a6d7efc0dc6a490d6ba39451b
Alvaro Tomasino <alvaroyaugusto@gmail.com>
2025-10-28 20:11:48 -0300

    [por arreglar] carga en la base de datos pero genera archivos de evidencia separados

--- src/pedido_agregacion/pedido_agregacion.routes.ts
+++ src/pedido_agregacion/pedido_agregacion.routes.ts
@@ -7,16 +7,5 @@ export const pedidos_agregacion_router = Router()
 
 pedidos_agregacion_router.get('/', verifyToken, authorizeRoles(['cazador', 'operador']), findAll)
 pedidos_agregacion_router.delete('/:id', verifyToken, authorizeRoles(['cazador', 'operador']), remove)
-pedidos_agregacion_router.post(
-  '/',
-  verifyToken,
-  authorizeRoles(['cazador', 'operador']),
-  upload.array('archivos', 5),
-  generarPedidosAgregacion
-)
-pedidos_agregacion_router.patch(
-  '/tomar-pedidos-agregacion/:id',
-  verifyToken,
-  authorizeRoles(['cazador', 'operador']),
-  tomarPedidosAgregacion
-)
+pedidos_agregacion_router.post('/', verifyToken, authorizeRoles(['cazador', 'operador']), upload.array('archivos', 10), generarPedidosAgregacion)
+pedidos_agregacion_router.patch('/tomar-pedidos-agregacion/:id', verifyToken, authorizeRoles(['cazador', 'operador']), tomarPedidosAgregacion)
