9f1ed60d734c32e992878998528c3bb361f33a72
Alvaro Tomasino <alvaroyaugusto@gmail.com>
2025-11-05 19:42:42 -0300

    CRUD completa

--- src/tests/integration.tipoanomalia.test.ts
+++ src/tests/integration.tipoanomalia.test.ts
@@ -4,6 +4,7 @@ import { app } from '../app.js'
 describe('API Integration Tests', () => {
   let request: any
   let token: string
+  let anomId: string
 
   beforeAll(async () => {
     request = supertest(app)
@@ -19,7 +20,7 @@ describe('API Integration Tests', () => {
     })
   })
 
-  describe('Tipo Anomalia Endpoints (GET y POST)', () => {
+  describe('Tipo Anomalia Endpoints (CRUD)', () => {
     test('GET /api/tipo_anomalia - debería devolver 200 y la lista de tipos de anomalías', async () => {
       const response = await request.get('/api/tipo_anomalia').set('authorization', `Bearer ${token}`).expect(200)
 
@@ -35,13 +36,19 @@ describe('API Integration Tests', () => {
       expect(response.status).toBe(500) // En nuestro sistema devuelve 500 si no lo encuentra, no 404
     })
 
-    test('GET /api/tipo_anomalia/:id - debería devolver 400 por formato de nombre inválido', async () => {
+    test('GET /api/tipo_anomalia - debería devolver 400 por formato de nombre inválido', async () => {
       const tipoInvalido = { nombre_tipo_anomalia: 'nomb re_invalido-123', dificultad_tipo_anomalia: 1 }
       const response = await request.post('/api/tipo_anomalia').set('authorization', `Bearer ${token}`).send(tipoInvalido)
       expect(response.status).toBe(400)
     })
 
-    test('POST /api/tipo_anomalia - debería crear y devolver un nuevo tipo_anomalia, si no está duplicado', async () => {
+    test('GET /api/tipo_anomalia - debería devolver 400 por formato de dificultad inválido', async () => {
+      const tipoInvalido = { nombre_tipo_anomalia: 'nombre valido', dificultad_tipo_anomalia: 5 }
+      const response = await request.post('/api/tipo_anomalia').set('authorization', `Bearer ${token}`).send(tipoInvalido)
+      expect(response.status).toBe(400)
+    })
+
+    test('POST /api/tipo_anomalia - debería crear y devolver un nuevo tipo_anomalia, si no está duplicado el nombre', async () => {
       const newTipo = {
         nombre_tipo_anomalia: 'Michelín Gigante',
         dificultad_tipo_anomalia: 3,
@@ -52,6 +59,7 @@ describe('API Integration Tests', () => {
       expect(response.body.data).toBeDefined()
       expect(response.body.data.nombre_tipo_anomalia).toBe(newTipo.nombre_tipo_anomalia)
       expect(response.body.data.dificultad_tipo_anomalia).toBe(newTipo.dificultad_tipo_anomalia)
+      anomId = response.body.data.id
     })
 
     test('POST /api/tipo_anomalia - debería devolver 400 cuando falta el nombre', async () => {
@@ -68,6 +76,24 @@ describe('API Integration Tests', () => {
       }
 
       await request.post('/api/tipo_anomalia').set('authorization', `Bearer ${token}`).send(invalidTipo).expect(500)
+    })
+
+    test('PUT /api/tipo_anomalia/:id - debería actualizar y devolver el tipo de anomalia existente', async () => {
+      const updatedTipo = {
+        nombre_tipo_anomalia: 'Michelín Actualizado',
+        dificultad_tipo_anomalia: 2,
+      }
+      const response = await request.put(`/api/tipo_anomalia/${anomId}`).set('authorization', `Bearer ${token}`).send(updatedTipo)
+
+      expect(response.body.data).toBeDefined()
+      expect(response.body.data.nombre_tipo_anomalia).toBe(updatedTipo.nombre_tipo_anomalia)
+      expect(response.body.data.dificultad_tipo_anomalia).toBe(updatedTipo.dificultad_tipo_anomalia)
+      expect(response.status).toBe(200)
+    })
+
+    test('DELETE /api/tipo_anomalia/:id - debería devolver 200 al eliminar un tipo de anomalia existente que no esté en un pedido', async () => {
+      const response = await request.delete(`/api/tipo_anomalia/${anomId}`).set('authorization', `Bearer ${token}`)
+      expect(response.status).toBe(200)
     })
   })
 })
